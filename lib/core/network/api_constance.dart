a77bd90d67520ab6110dd23cccfd203ecc95398e
MazenAymann <mazenaymanmo22@gmail.com>
2024-07-13 19:31:18 +0300

    Movies Screen

--- lib/core/network/api_constance.dart
+++ lib/core/network/api_constance.dart
@@ -8,4 +8,10 @@ class ApiConstance {
   static const String popularMoviesPath = '$baseUrl/movie/popular?$apiKey';
 
   static const String topRatedMoviesPath = '$baseUrl/movie/top_rated?$apiKey';
+
+  static const String baseImageUrl = 'https://image.tmdb.org/t/p/w500/';
+
+  static String imageUrl(String path) {
+    return baseImageUrl + path;
+  }
 }
